1c650298b6fe8bd0c5e69a4b88c3c806f95d0bee
Rodrigobr2003 <ronaldobr2003@gmail.com>
2024-04-19 18:23:38 -0300

    :tada: Primeiro commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,6 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
-export class AppComponent {
-  title = 'gestao_lab';
-}
+export class AppComponent {}
