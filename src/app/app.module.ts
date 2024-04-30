72c9193bed3787cfdff114a1ba18e09b663ef0bd
Rodrigobr2003 <ronaldobr2003@gmail.com>
2024-04-30 12:24:50 -0300

    :recycle: refactor: Ajusta organização de pastas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HomeLayoutComponent } from './layout/home-layout/home-layout.component';
-import { UserLayoutComponent } from './layout/user-layout/user-layout.component';
-import { LabLayoutComponent } from './layout/lab-layout/lab-layout.component';
-import { CalendarioComponent } from './layout/lab-layout/calendario/calendario.component';
-import { HorariosComponent } from './layout/lab-layout/calendario/horarios/horarios.component';
-import { ApelarComponent } from './layout/apelar/apelar.component';
+import { HomeLayoutComponent } from './layout/display/home-layout/home-layout.component';
+import { UserLayoutComponent } from './layout/display/user-layout/user-layout.component';
+import { LabLayoutComponent } from './layout/display/lab-layout/lab-layout.component';
+import { HorariosComponent } from './layout/display/lab-layout/calendario/horarios/horarios.component';
+import { ApelarComponent } from './layout/display/apelar/apelar.component';
+import { DisplayComponent } from './layout/display/display.component';
+import { CalendarioComponent } from './layout/display/lab-layout/calendario/calendario.component';
 
 @NgModule({
   declarations: [
@@ -16,9 +17,10 @@ import { ApelarComponent } from './layout/apelar/apelar.component';
     HomeLayoutComponent,
     UserLayoutComponent,
     LabLayoutComponent,
-    CalendarioComponent,
     HorariosComponent,
     ApelarComponent,
+    DisplayComponent,
+    CalendarioComponent,
   ],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
---
+++ src/app/layout/display/display.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-display',
+  templateUrl: './display.component.html',
+  styleUrl: './display.component.css'
+})
+export class DisplayComponent {
+
+}
