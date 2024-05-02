15df15fc6b3a4f496cea1ecac5fe3c7df70b0ccf
Rodrigobr2003 <ronaldobr2003@gmail.com>
2024-05-02 12:31:20 -0300

    :refactor: refactor: Ajusta pastas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,26 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HomeLayoutComponent } from './layout/display/home-layout/home-layout.component';
-import { UserLayoutComponent } from './layout/display/user-layout/user-layout.component';
+import { HomeComponent } from './layout/display/home/home.component';
 import { LabLayoutComponent } from './layout/display/navbar/lab-layout/lab-layout.component';
 import { ApelarComponent } from './layout/display/apelar/apelar.component';
 import { DisplayComponent } from './layout/display/display.component';
 import { NavbarComponent } from './layout/display/navbar/navbar.component';
 import { CalendarioComponent } from './layout/display/navbar/lab-layout/calendario/calendario.component';
 import { HorariosComponent } from './layout/display/navbar/lab-layout/calendario/horarios/horarios.component';
+import { LoginComponent } from './layout/display/login/login.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeLayoutComponent,
-    UserLayoutComponent,
     LabLayoutComponent,
     ApelarComponent,
     DisplayComponent,
     NavbarComponent,
     CalendarioComponent,
     HorariosComponent,
+    HomeComponent,
+    LoginComponent,
   ],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
