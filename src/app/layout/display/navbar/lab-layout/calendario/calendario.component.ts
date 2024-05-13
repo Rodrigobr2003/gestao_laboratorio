9bc6be028415b9f66d1201d57f9d1141d7ebc9be
Rafael <rafavmaximo@gmail.com>
2024-05-13 12:01:55 -0300

    :sparkles: feat: Header

--- src/app/layout/display/navbar/lab-layout/calendario/calendario.component.ts
+++ src/app/layout/display/navbar/lab-layout/calendario/calendario.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { faCalendarDays } from '@fortawesome/free-solid-svg-icons';
 @Component({
   selector: 'app-calendario',
   templateUrl: './calendario.component.html',
   styleUrl: './calendario.component.css'
 })
-export class CalendarioComponent {
+export class CalendarioComponent implements OnInit{
 
+  id: number
+
+  constructor(private route: ActivatedRoute){}
+
+  ngOnInit(): void {
+      this.id = this.route.snapshot.params['id']
+  }
+
+  calendario = faCalendarDays;
+
+  monthAno : string [] = [
+    "Janeiro", "Fevereiro", "Março", "Abril", "Maio", "Junho",
+  "Julho", "Agosto", "Setembro", "Outubro", "Novembro", "Dezembro"
+  ];
 }
