b68a2f0e099393d8b3df2238dead608404c528a7
Rafael <rafavmaximo@gmail.com>
2024-05-26 16:18:38 -0300

    :sparkles: feat: Mudança de cores e contagem dos dias

--- src/app/layout/display/navbar/lab-layout/calendario/calendario.component.ts
+++ src/app/layout/display/navbar/lab-layout/calendario/calendario.component.ts
@@ -1,30 +1,49 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { faCalendarDays } from '@fortawesome/free-solid-svg-icons';
+
 @Component({
   selector: 'app-calendario',
   templateUrl: './calendario.component.html',
-  styleUrl: './calendario.component.css'
+  styleUrls: ['./calendario.component.css']
 })
-export class CalendarioComponent implements OnInit{
+export class CalendarioComponent implements OnInit {
 
-  id: number
-
-  constructor(private route: ActivatedRoute){}
-
-  ngOnInit(): void {
-      this.id = this.route.snapshot.params['id']
-  }
-
+  id: number;
   calendario = faCalendarDays;
 
-  monthAno : string [] = [
+  monthAno: string[] = [
     "Janeiro", "Fevereiro", "Março", "Abril", "Maio", "Junho",
-  "Julho", "Agosto", "Setembro", "Outubro", "Novembro", "Dezembro"
+    "Julho", "Agosto", "Setembro", "Outubro", "Novembro", "Dezembro"
   ];
-
-  times = [1, 2, 3, 4];
 
   daysOfWeek = ['Seg', 'Ter', 'Quar', 'Quin', 'Sex'];
   startDate = new Date(2024, 0, 1);
+
+  weeks: { date: string, day: string, isColorChanged: boolean }[][] = [];
+
+  constructor(private route: ActivatedRoute) { }
+
+  ngOnInit(): void {
+    this.id = this.route.snapshot.params['id'];
+    this.initializeDays();
+  }
+
+  initializeDays() {
+    const currentDate = new Date(this.startDate);
+    for (let i = 0; i < 4; i++) {
+      const week: { date: string, day: string, isColorChanged: boolean }[] = [];
+      for (let j = 0; j < this.daysOfWeek.length; j++) {
+        const day = this.daysOfWeek[j];
+        const date = `${('0' + currentDate.getDate()).slice(-2)}/${('0' + (currentDate.getMonth() + 1)).slice(-2)}`;
+        week.push({ day, date, isColorChanged: false });
+        currentDate.setDate(currentDate.getDate() + 1);
+      }
+      this.weeks.push(week);
+    }
+  }
+
+  changeColor(day: { date: string, day: string, isColorChanged: boolean }) {
+    day.isColorChanged = !day.isColorChanged;
+  }
 }
