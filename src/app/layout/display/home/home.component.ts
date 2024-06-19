902aad48410f65561c627d1684170e2215442cdf
Rodrigobr2003 <ronaldobr2003@gmail.com>
2024-06-19 17:40:16 -0300

    :sparkles: feat: Adiciona conteúdo Home

--- src/app/layout/display/home/home.component.ts
+++ src/app/layout/display/home/home.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
+import { faTriangleExclamation } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
+  styleUrl: './home.component.css',
 })
 export class HomeComponent {
-
+  aviso = faTriangleExclamation;
 }
