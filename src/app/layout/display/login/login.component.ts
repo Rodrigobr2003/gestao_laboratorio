bd28b33223e2d48985ede35a27354a55458661e3
Rodrigobr2003 <ronaldobr2003@gmail.com>
2024-06-19 16:46:12 -0300

    :sparkles: feat: Adiciona navegação do login

--- src/app/layout/display/login/login.component.ts
+++ src/app/layout/display/login/login.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrl: './login.component.css'
+  styleUrl: './login.component.css',
 })
 export class LoginComponent {
+  router = inject(Router);
 
+  accessSite() {
+    this.router.navigate(['/home']);
+  }
 }
